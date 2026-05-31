a3d614e6f7b39e254300fae1381af8078ca8d6db
MohsinKhan-29 <mohsinkhan292003@gmail.com>
2026-05-31 23:49:35 +0500

    Update project

--- Server/src/utils/sendEmail.js
+++ Server/src/utils/sendEmail.js
@@ -3,24 +3,24 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    // const transporter = nodemailer.createTransport({
-    //   service: "gmail",
-    //   auth: {
-    //     user: process.env.EMAIL_USER,
-    //     pass: process.env.EMAIL_PASS,
-    //   },
-    // });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-    // const mailOptions = {
-    //   from: `"DigitalAgency" <${process.env.EMAIL_USER}>`,
-    //   to,
-    //   subject,
-    //   html,
-    // };
+    const mailOptions = {
+      from: `"DigitalAgency" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    };
 
-    // await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
 
-    console.log("Email sent successfully");
+    console.log("subcriber Email sent successfully");
   } catch (error) {
     console.log("Email error:", error);
   }
