48782260fdf22e3af2564080eddfc22af49f4133
MohsinKhan-29 <mohsinkhan292003@gmail.com>
2026-06-01 00:03:55 +0500

    Update project

--- Server/src/utils/sendEmail.js
+++ Server/src/utils/sendEmail.js
@@ -5,9 +5,10 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    pass: process.env.EMAIL_PASS
+  }
 });
+
 
 // Optional: verify connection once at startup
 transporter.verify((error, success) => {
