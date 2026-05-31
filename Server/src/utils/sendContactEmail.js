b3981dbc2be220635466b3db7a6aa2379f930518
MohsinKhan-29 <mohsinkhan292003@gmail.com>
2026-05-31 23:46:49 +0500

    Update project

--- Server/src/utils/sendContactEmail.js
+++ Server/src/utils/sendContactEmail.js
@@ -1,20 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ subject, html }) => {
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
+  //   const transporter = nodemailer.createTransport({
+  //     service: "gmail",
+  //     auth: {
+  //       user: process.env.EMAIL_USER,
+  //       pass: process.env.EMAIL_PASS,
+  //     },
+  //   });
 
-//   await transporter.sendMail({
-//     from: `"Contact Form" <${process.env.EMAIL_USER}>`,
-//     to: process.env.ADMIN_EMAIL,
-//     subject,
-//     html,
-//   });
+  //   await transporter.sendMail({
+  //     from: `"Contact Form" <${process.env.EMAIL_USER}>`,
+  //     to: process.env.ADMIN_EMAIL,
+  //     subject,
+  //     html,
+  //   });
+
+  console.log("Email sent successfully");
 };
 
 module.exports = sendEmail;
