818207a52646e2a70782c3a290ec7c05a053c13f
MohsinKhan-29 <mohsinkhan292003@gmail.com>
2026-06-01 00:24:56 +0500

    Update project

--- Server/src/utils/sendEmail.js
+++ Server/src/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const data = await resend.emails.send({
-      from: "DigitalAgency <mohsinkhan292003@gmail.com>",
+      from: "DigitalAgency <onboarding@resend.dev>",
       to,
       subject,
       html,
